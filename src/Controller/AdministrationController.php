e84637c1fb165b867e6e3b66b4234c20e04c044a
tristanpommery <tristan.pommery@gmail.com>
2019-10-28 16:43:35 +0100

    pages

---
+++ src/Controller/AdministrationController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdministrationController extends AbstractController
+{
+
+    public function adminIndex()
+    {
+        return $this->render('administration/index.html.twig');
+    }
+    public function edit()
+    {
+        return $this->render('administration/edit.html.twig');
+    }
+    public function delete()
+    {
+        return $this->render('administration/delete.html.twig');
+    }
+    public function add()
+    {
+        return $this->render('administration/add.html.twig');
+    }
+
+}
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,13 +7,31 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    /**
-     * @Route("/main", name="main")
-     */
-    public function index()
+
+    public function home()
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('main/index.html.twig');
     }
+
+    public function products()
+    {
+        return $this->render('main/products.html.twig');
+    }
+
+    public function discount()
+    {
+        return $this->render('main/discount.html.twig');
+    }
+
+    public function support()
+    {
+        return $this->render('main/support.html.twig');
+    }
+
+    public function about()
+    {
+        return $this->render('main/about.html.twig');
+    }
+
+
 }
